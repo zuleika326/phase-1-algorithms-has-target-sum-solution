b267d47c686bd57893d248bbeb08b0352b52edbc
zuleika <njokizuleika@gmail.com>
2023-04-17 13:22:28 +0300

    done has target sum lab solution

--- index.js
+++ index.js
@@ -1,6 +1,18 @@
 function hasTargetSum(array, target) {
-  // Write your algorithm here
+  for (let i = 0; i < array.length; i++) {
+    // for the current number, identify a complementary number that adds to our target
+    // (for example: if our number is 2, and the target is 5, the complementary number is 3)
+    const complement = target - array[i];
+    // iterate over the remaining numbers in the array
+    for (let j = i + 1; j < array.length; j++) {
+      // check if any of the remaining numbers is the complement
+      // if so, return true
+      if (array[j] === complement) return true;
+    }
+  }
+  return false;
 }
+
 
 /* 
   Write the Big O time complexity of your function here
